24363e897cd0ac700145048ad875526616ed37f4
ronykhan95 <farjilul.alam@gmail.com>
2021-05-09 22:34:28 +0600

    first commit

---
+++ src/components/NoMatch/NoMatch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NoMatch = () => {
+    return (
+        <div>
+            <p>this route is not define.</p>
+        </div>
+    );
+};
+
+export default NoMatch;
